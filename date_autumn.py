c82b37216c4d00d3cf19994f32c66e18644914fc
kev1e <57797330+kev1e@users.noreply.github.com>
2020-10-29 21:52:59 +0300

    1
    1

--- date_autumn.py
+++ date_autumn.py
@@ -1,21 +1,9 @@
 def date_autumn(dates):
-    today = "10-22-2020"
-    today_ = today.split("-")
-    autumn_days = []
-    answer = []
-    min_month = 1000000
-    min_year = 1000000
-    min_day = 1000000
-    for i in range(len(dates)):
-        data = dates[i].split("-")
-        if data[0] == "09" or data[0] == "10" or data == "11":
-            autumn_days.append(data)
-    for i in range(len(autumn_days)):
-        x = autumn_days[i].split('-')
-        if (abs(int(x[i][0]) - int(today_[0])) <= min_month) and (abs(int(x[i][2]) - int(today_[2])) <= min_year) and (
-                abs(int(x[i][1]) - int(today_[1])) <= min_day):
-            min_month = abs(int(x[i][0]) - int(today_[0]))
-            min_day = abs(int(x[i][1]) - int(today_[1]))
-            min_year = abs(int(x[i][2]) - int(today_[2]))
-            answer.append(i)
-    return answer
+    days = list(dates)
+    correct_month = []
+    for i in range(len(days)):
+        x = days[i].split('-')
+        if x[0] == "09" or x[0] == "10" or x[0] == "11":
+            correct_month.append(x)
+    ans = sorted(correct_month, key=lambda x: (x[2], x[1], x[0]))
+    return "-".join(ans[-1])
